a48187c874b4dc265704bf2a6dd42359f3e19dc3
minax99 <minacheth@gmail.com>
2025-10-20 11:39:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'CHAMA-MD=2N82GBgL#xy8cu78CRD4fBEdjvzpqXSarQs1taNVhSivbv-FexYY',
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94772194789",
+SESSION_ID: process.env.SESSION_ID || 'CHAMA-MD=qVc1hLJA#T9G62mxrV2-H80Qaj4QwkJ_KLgPBayzdHpiHipCy8GA',
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94779510013",
 ALIVE: process.env.ALIVE || `default`,
-OWNER_NAME: process.env.OWNER_NAME || 'chamindu' ,     
+OWNER_NAME: process.env.OWNER_NAME || 'Chethmina' ,     
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 PREFIX:  process.env.PREFIX || ['.'] ,
-FOOTER: '> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴀᴅʜɪ ᴏꜰᴄ',
+FOOTER: process.env.FOOTER || '> 👨🏻‍💻 *ᴄʜᴇᴛʜᴍɪɴᴀ ᴋᴀᴠɪꜱʜᴀɴ*',
 DIRECTION: true,
-IMAGE: process.env.IMAGE || `https://i.ibb.co/B25Ws4nf/temp-image.jpg`,
-LOGO: process.env.LOGO || `https://i.ibb.co/B25Ws4nf/temp-image.jpg`     
+IMAGE: process.env.IMAGE || `https://files.catbox.moe/h6t2am.jpg`,
+LOGO: process.env.LOGO || `https://files.catbox.moe/h6t2am.jpg`     
 };
